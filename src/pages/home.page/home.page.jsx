cebb18dff6a6a0be38280577f33e69d3c6162ac9
Ba Duy <nguyenvubaduy9a11a@gmail.com>
2024-12-24 16:08:54 +0700

    final(1)

--- src/pages/home.page/home.page.jsx
+++ src/pages/home.page/home.page.jsx
@@ -5,45 +5,52 @@ const HomePage = () => {
     const navigate = useNavigate()
 
     return (
-        <div className="home-page"
-            style={{
-                height: "100vh",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "space-around",
-                width: "800px",
-                margin: "0 auto"
-            }}>
-            <div className="login"
+        <>
+            <div className="home-page"
                 style={{
-                    padding: "30px 50px",
-                    fontSize: "56px",
-                    backgroundColor: "blue",
-                    color: "white",
-                    display: "inline",
-                    borderRadius: "20px",
-                    cursor: "pointer"
-                }}
-                onClick={() => { navigate("/login") }}
-            >
-                Login
-            </div>
+                    height: "100vh",
+                    width: "800px",
+                    margin: "0 auto",
+                    display: "flex",
+                    justifyContent: "space-around",
+                    flexDirection: "column"
+                }}>
 
-            <div className="register"
-                style={{
-                    padding: "30px 50px",
-                    fontSize: "56px",
-                    backgroundColor: "green",
-                    color: "white",
-                    display: "inline",
-                    borderRadius: "20px",
-                    cursor: "pointer"
-                }}
-                onClick={() => { navigate("/register") }}
-            >
-                Register
+                <h1 style={{ textAlign: "center" }}>CLICK TO VIEW</h1>
+
+                <div style={{ display: "flex", justifyContent: "space-between" }}>
+                    <div className="login"
+                        style={{
+                            padding: "30px 50px",
+                            fontSize: "56px",
+                            backgroundColor: "blue",
+                            color: "white",
+                            display: "inline",
+                            borderRadius: "20px",
+                            cursor: "pointer"
+                        }}
+                        onClick={() => { navigate("/login") }}
+                    >
+                        Login
+                    </div>
+
+                    <div className="register"
+                        style={{
+                            padding: "30px 50px",
+                            fontSize: "56px",
+                            backgroundColor: "green",
+                            color: "white",
+                            display: "inline",
+                            borderRadius: "20px",
+                            cursor: "pointer"
+                        }}
+                        onClick={() => { navigate("/register") }}
+                    >
+                        Register
+                    </div>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
